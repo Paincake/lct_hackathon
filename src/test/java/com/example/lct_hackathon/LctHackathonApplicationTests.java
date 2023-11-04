d2499bc607af8cf5943a0da6caa6c8698cf9c589
Paincake <ryazhev2012@gmail.com>
2023-11-04 13:17:34 +0300

    Initial commit

---
+++ src/test/java/com/example/lct_hackathon/LctHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lct_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LctHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
