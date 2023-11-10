36d328d83cc481235292472144649d814a2862eb
Paincake <ryazhev2012@gmail.com>
2023-11-11 00:32:23 +0300

    cors.

--- src/main/java/com/example/lct_hackathon/config/WebSecurityConfig.java
+++ src/main/java/com/example/lct_hackathon/config/WebSecurityConfig.java
@@ -28,7 +28,22 @@ public class WebSecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+		configuration.setAllowedOrigins(Arrays.asList("https://example.com", 
+        "https://localhost:8080", 
+        "http://localhost:8080",
+        "http://localhost:3000",
+        "https://localhost:3000",
+        "https://localhost:8081", 
+        "http://localhost:8081",
+        "http://94.139.254.37:8081",
+        "https://94.139.254.37:8081",
+        "http://94.139.254.37:8080",
+        "https://94.139.254.37:8080",
+        "http://94.139.254.37:2000",
+        "https://94.139.254.37:2000",
+        "http://94.139.254.37:3000",
+        "https://94.139.254.37:3000"));
+
 		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
