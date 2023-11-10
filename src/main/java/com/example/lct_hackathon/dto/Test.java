92adcaee6ff7bdc15cd51a5341378ea26850aa53
Paincake <ryazhev2012@gmail.com>
2023-11-11 00:43:59 +0300

    local web testing

--- src/main/java/com/example/lct_hackathon/dto/Test.java
+++ src/main/java/com/example/lct_hackathon/dto/Test.java
@@ -3,9 +3,15 @@ package com.example.lct_hackathon.dto;
 import java.sql.Timestamp;
 import java.time.Instant;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Timestamp.from(Instant.now()));
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println("менеджер");
+        System.out.println(bCryptPasswordEncoder.encode("manager"));
+        System.out.println("не менеджер");
+        System.out.println(bCryptPasswordEncoder.encode("agent"));
     }
     
 }
